0f070f45e36129434beba706854acb4fd2da3fbf
David07994415 <david.game.power@gmail.com>
2024-10-02 21:53:11 +0800

    Feat:Add View Component into Core_MVC Project

---
+++ Core_MVC/ViewComponents/EmployeeViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Core_MVC.GetApiLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_MVC.ViewComponents
+{
+    public class EmployeeViewComponent : ViewComponent
+    {
+
+        private readonly ApiList _apiList;
+
+        // 進行 DI 注入 
+        public EmployeeViewComponent(ApiList apiList)
+        {
+            _apiList = apiList;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string TestParam = null)
+        {
+            var data = await _apiList.GetApiConnectionData();
+            // 預設資料夾：/Views/Shared/Components/{檢視元件名稱}/{檢視名稱}
+
+            if (TestParam != null)
+            {
+                ViewBag.TestPara = TestParam;
+            }
+            return View(data);
+        }
+
+    }
+}
